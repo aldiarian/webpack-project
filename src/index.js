cf9a485a52ad46bdc4a02688916deefd771cbc1c
alberto diarian <alberto.diarian@unidadeditorial.es>
2019-12-16 10:43:11 +0100

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
+require("babel-polyfill");
+
 /* css */
 import '../src/scss/estilos.scss'
 
 // JS
-import otro from "./modulos/js/otro";
-otro()
+import Search from './modulos/js/search';
 
+/** Estado Global de la aplicación
+    - objeto buscar
+    - objeto receta actual
+    - objeto lista de la compra
+    - objeto recetas con liks
+**/
+const state = {
 
-// misdatos
-// import misdatos from "./modulos/data/misdatos";
-// console.log(misdatos);
+}
+
+const controlSearch = async ()=>{
+    // 1) Tomar la búsqueda de la vista
+    const query = 'pizza';
+    
+    if(query){
+        // 2) Nueva búsqueda y añadirla en el estado
+        state.search = new Search(query);
+
+        // 3) Preparar la UI para los resultados
+
+        // 4) Buscar recetas
+        await state.search.getResult(); // la búsqueda es asíncrona
+
+        // 5) Renderizar los resultados 
+        console.log(state.search.results);
+        
+    }
+}
+
+document.querySelector('.buscador').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+    
+})
+
 
