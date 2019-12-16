b14f7cd8baa69270d96188c07c99683fc2c8d9bb
alberto diarian <alberto.diarian@unidadeditorial.es>
2019-12-16 15:03:36 +0100

    search view

--- src/modulos/js/views/searchView.js
+++ src/modulos/js/views/searchView.js
@@ -4,11 +4,15 @@ export const getInput = () => elements.searchInput.value;
 
 const rederRecipe = recipe =>{
     const markup = `
-    <li>
-        <p>Nombre</p>
-        <p>Listo en : xxxx minutos</p>
-        <p>Para 6 personas</p>
-        <img class="receta-img" src="xx" alt="">
-    </li>
-    `;
+        <li>
+            <p>Nombre</p>
+            <p>Listo en : xxxx minutos</p>
+            <p>Para 6 personas</p>
+            <img class="receta-img" src="xx" alt="">
+        </li>
+        `;
+}
+
+export const rederResults = recipes => {
+    recipes.forEach(renderRecipe);
 }
